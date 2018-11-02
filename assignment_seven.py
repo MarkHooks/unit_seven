0e4e101f940271eb2b768a1e2eaf93955e24dca9
Mark <mark@iMac2.local>
2018-11-02 11:17:48 -0400

    finnished project

--- assignment_seven.py
+++ assignment_seven.py
@@ -1,16 +1,32 @@
+# Mark Hooks 11/2/18
+# this program is to encode and decode a phrase given by the user.
+
+
 def original_alphabet():
-    alphabet = "a b c d e f g h i j k l m n o p q r s t u v w x y z"
+    """
+    this is to create an alphabet
+    :return: this returns the function
+    """
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
     return alphabet
 
 
 def key():
-    user_key = int(input("what should the rotation of the alphabet be (0-25)"))
+    """
+    this gets the key from the user
+    :return: this returns the function
+    """
+    user_key = int(input("what should the rotation of the alphabet be (0-25) "))
     return user_key
 
 
 def user_encode():
+    """
+    this asks if the user wants to encode a phrase or decode a phrase
+    :return: this returns the function
+    """
     while True:
-        question = input("press e to encode, press d to decode, press q to quit.")
+        question = input("press e to encode, press d to decode, press q to quit. ")
         if question == "e":
             return
         elif question == "d":
@@ -21,42 +37,86 @@ def user_encode():
 
 
 def user_phrase():
-    phrase = input("please type in the phrase you want to encode")
-    phrase.lower()
-    phrase = phrase.replace(" ", "")
+    """
+    this gets the user's phrase
+    :return: this returns the function
+    """
+    phrase = input("please type in the phrase you want to encode/decode.")
+    phrase = phrase.lower()
     return phrase
 
 
 def new_alphabet(alphabet, user_key):
+    """
+    this is to create a new alphabet based on the key
+    :param alphabet: this is the original alphabet
+    :param user_key: this is the key that is given by the user
+    :return: this returns the function
+    """
     first = alphabet[:user_key]
     last = alphabet[user_key:]
     nw_alphabet = last + first
     return nw_alphabet
 
 
-def encode(phrase, nw_alphabet):
+def encode(phrase, nw_alphabet, alphabet):
+    """
+    this is to encode the user's phrase
+    :param phrase: this is the original phrase
+    :param nw_alphabet: this is the new alphabet based on the user's key
+    :param alphabet: this is the original alphabet
+    :return: this returns the function
+    """
     new_phrase = ""
     for letter in phrase:
-        if letter == " ":
+        if letter not in alphabet:
             new_phrase = new_phrase + letter
         else:
-            index = nw_alphabet.index(letter)
+            index = alphabet.index(letter)
             new_letter = nw_alphabet[index]
-            new_phrase = phrase + new_letter
+            new_phrase = new_phrase + new_letter
     return new_phrase
 
 
+def decode(phrase, nw_alphabet, alphabet):
+    """
+    this is to decode the user's phrase
+    :param phrase: this is the user's phrase
+    :param nw_alphabet: this is the new alphabet based on the user's key
+    :param alphabet: this is the original alphabet
+    :return: this returns the function
+    """
+    nw_phrase = ""
+    for letter in phrase:
+        if letter not in nw_alphabet:
+            nw_phrase = nw_phrase + letter
+        else:
+            index = nw_alphabet.index(letter)
+            new_letter = alphabet[index]
+            nw_phrase = nw_phrase + new_letter
+    return nw_phrase
+
+
 def main():
-    alphabet = original_alphabet()
-    user_phrase()
-    user_key = key()
-    nw_alphabet = new_alphabet(alphabet, user_key)
-    question = input("press e to encode, press d to decode, press q to quit.")
+    question = input("press e to encode, press d to decode, press q to quit:")
     if question == "e":
+        # this encodes the phrase
+        alphabet = original_alphabet()
+        user_key = key()
+        nw_alphabet = new_alphabet(alphabet, user_key)
         phrase = user_phrase()
-        encode(phrase, nw_alphabet)
+        new_phrase = encode(phrase, nw_alphabet, alphabet)
+        print(new_phrase)
     elif question == "d":
-        pass
+        # this decodes the phrase
+        alphabet = original_alphabet()
+        user_key = key()
+        nw_alphabet = new_alphabet(alphabet, user_key)
+        phrase = user_phrase()
+        nw_phrase = decode(phrase, nw_alphabet, alphabet)
+        print(nw_phrase)
+    elif question == "q":
+        print("thanks for playing")
     else:
         pass
 
